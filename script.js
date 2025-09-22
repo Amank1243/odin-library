8d65cdd143c934c2bceb6b1f7f8ad1972ca306a1
Aman Khajuria <24khajuriaa@gmail.com>
2025-09-21 23:09:22 -0700

    Worked on proj a tiny bit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* DELETE WHEN DONE WITH PROJECT
+Features:
+    Add a create book button
+    Books which grow if author or book is long
+    Seperate menu in which you input book info, which blurs the background
+    Custom growing book background which grows or shrinks depending on how many books there are
+    Add an edit book button, where you can edit info for already made books
+    Add a delete button for the book
+*/
+
 const MYLIBRARY = [];
 
 const BUTTON = document.getElementById("add-book")
@@ -16,7 +26,13 @@ function Book(title, author, numberOfPages, haveRead) {
     id = crypto.randomUUID();
 }
 
-function addBookToLibrary(book) {
-    newBook = new Book(book.title, book.author, book.numberOfPages, book.haveRead)
+function addBookToLibrary(title, author, numberOfPages, haveRead) {
+    newBook = new Book(d)
     MYLIBRARY.push(newBook);
+}
+
+add 
+
+function openBookInfoModal() {
+
 }
