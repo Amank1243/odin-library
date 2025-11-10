e41dd0355c8bd8a8fc2552f3dcfa6863862c0a51
Aman Khajuria <24khajuriaa@gmail.com>
2025-11-10 11:00:13 -0800

    chore: Removed unnecessary whitespace & lines

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-/* DELETE WHEN DONE WITH PROJECT
-Features:
-    Add a create book button
-    Books which grow if author or book is long
-    Seperate menu in which you input book info, which blurs the background
-    Custom growing book background which grows or shrinks depending on how many books there are
-    Add an edit book button, where you can edit info for already made books
-    Add a delete button for the book
- */
-
 const MYLIBRARY = [];
-
 const BUTTON = document.getElementById("add-book");
 const BOOKCONTAINER = document.querySelector(".library-container");
 const MODUAL = document.querySelector("dialog");
@@ -24,15 +13,19 @@ function Book(title, author, numberOfPages, haveRead) {
     let id = crypto.randomUUID();
 }
 
+let book1 = new Book("The Alchemist", "Paulo Coelho", 208, true);
+MYLIBRARY.push(book1);
+
+let book2 = new Book("The Power of a Habit", "Charles Duhigg", 416, true);
+MYLIBRARY.push(book2);
+
 function addBookToLibrary() {
     let title = document.getElementById("book").value;
     let author = document.getElementById("author").value;
     let numberOfPages = document.getElementById("pages").value;
-    let selectedIndex = document.querySelector("select").selectedIndex;
     let haveRead = document.querySelector("select").value;
 
     let book = new Book(title, author, numberOfPages, haveRead);
-    console.log(book);
     MYLIBRARY.push(book);  
 }
 
@@ -45,7 +38,7 @@ function displayBook() {
             book.className = "book";
             BOOKCONTAINER.appendChild(book);
 
-            book.innerText = document.get
+            book.innerText = displayBook;
         }
 
         firstTime = false;
@@ -62,11 +55,6 @@ function displayBook() {
     }
 }
 
-
-let book1 = new Book("The Alchemist", "Paulo Coelho", 208, true);
-MYLIBRARY.push(book1);
-let book2 = new Book("The Power of a Habit", "Charles Duhigg", 416, true);
-MYLIBRARY.push(book2);
 BUTTON.addEventListener("click", () => {
     MODUAL.showModal();
 });
