ad6ff75d863a715289238687eba05f7d883e1eee
Aman Khajuria <24khajuriaa@gmail.com>
2026-01-26 20:33:01 -0800

    feat: implementing functionality to cloned buttons
    Figure out how to add function to the newly cloned buttons. Once this is done, the next step is to figure out how the deleting process is going to work

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const MYLIBRARY = [];
 const BUTTON = document.getElementById("add-book");
 const BOOKCONTAINER = document.querySelector(".library-container");
-const BOOK = document.querySelector(".book")
+const BOOK = document.querySelector(".book");
 const MODAL = document.querySelector("dialog");
 const SUBMITBUTTON = document.querySelector(".dialog-submit-button");
+const DELETEBUTTON = document.querySelector(".deleteButton");
+
 
 function Book(title, author, numberOfPages, haveRead, id) {
-    this.title = title;
-    this.author = author;
+    this.title = title;    this.author = author;
     this.numberOfPages = numberOfPages;
     this.haveRead = haveRead;
     this.id = crypto.randomUUID();
@@ -15,22 +16,23 @@ function Book(title, author, numberOfPages, haveRead, id) {
 
 let book1 = new Book("The Alchemist", "Paulo Coelho", 208, true);
 MYLIBRARY.push(book1);
-displayBook(book1)
+displayBook(book1);
 
 let book2 = new Book("The Power of a Habit", "Charles Duhigg", 416, true);
 MYLIBRARY.push(book2);
-displayBook(book2)
+displayBook(book2);
 
 function displayBook(book) {
-    let newBook = BOOK.cloneNode(true)
+    let newBook = BOOK.cloneNode(true);
     
-    newBook.querySelector(".row .getBook").textContent = book.title
-    newBook.querySelector(".row .getAuthor").textContent = book.author
-    newBook.querySelector(".row .getPages").textContent = book.numberOfPages
-    newBook.querySelector(".row .getHasRead").textContent = book.haveRead
-    newBook.style.display = ""
-
-    BOOKCONTAINER.appendChild(newBook)
+    newBook.querySelector(".row .getBook").textContent = book.title;
+    newBook.querySelector(".row .getAuthor").textContent = book.author;
+    newBook.querySelector(".row .getPages").textContent = book.numberOfPages;
+    newBook.querySelector(".row .getHasRead").textContent = book.haveRead;
+    
+    newBook.style.display = "";
+    newBook.dataset.id = book.id;
+    BOOKCONTAINER.appendChild(newBook);
 }
 
 function addBookToLibrary() {
@@ -44,6 +46,10 @@ function addBookToLibrary() {
     
     displayBook(book);
 }
+// fix: figure out how to add functionality to cloned buttons
+DELETEBUTTON.addEventListener("click", () => {
+    console.log("I was clicked");
+});
 
 BUTTON.addEventListener("click", () => {
     MODAL.showModal();
